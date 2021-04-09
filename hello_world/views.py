5ddf20fd1f46bdeca80d4e16660fcfc7fc2e6c3e
Bhagyashri <Bhagyashri@email.com>
2021-04-09 14:00:58 +0530

    My First commit

---
+++ hello_world/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def show(request):
+    return HttpResponse('Hello this is Bhagyashri Talele am from Batch T4 and my prn is 1942209')
